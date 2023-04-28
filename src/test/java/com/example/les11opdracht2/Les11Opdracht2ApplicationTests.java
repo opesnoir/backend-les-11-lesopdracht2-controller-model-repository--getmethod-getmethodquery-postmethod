99d08d3ad4e855ff850a7a629aa51c67cef2b863
opesnoir <goodsurroundsme@outlook.com>
2023-04-28 16:20:00 +0200

    packadges aangemaakt voor controller, model en repository, packadge naam = enkelvoud

---
+++ src/test/java/com/example/les11opdracht2/Les11Opdracht2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.les11opdracht2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les11Opdracht2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
