fbf1e6fa6036c3cc249681548aff59fdf26efc3f
opesnoir <goodsurroundsme@outlook.com>
2023-04-30 11:44:53 +0200

    postmapping met URI componentbuilder toegevoegd

--- src/main/java/com/example/les11opdracht2/controller/StudentController.java
+++ src/main/java/com/example/les11opdracht2/controller/StudentController.java
@@ -4,9 +4,10 @@ import com.example.les11opdracht2.model.Student;
 import com.example.les11opdracht2.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/students") //hiermee geef je de root-path van alle endpoints aan, alle paden die we definiëren beginnen met de root-path /students
@@ -20,4 +21,19 @@ public class StudentController {
     public ResponseEntity<Iterable<Student>> getStudents(){
         return ResponseEntity.ok(repos.findAll());
     }
+
+    //student toevoegen door Postmapping
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student s){
+        repos.save(s);
+        //het volgende mag je qua schrijfmethode gewoon aannemen
+        URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/"+ s.getId().toUriString()));
+        return ResponseEntity.created(uri).body(s);
+    }
+
+
+
+
 }
